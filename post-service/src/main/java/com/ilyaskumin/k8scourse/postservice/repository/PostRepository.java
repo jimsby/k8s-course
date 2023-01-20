1d55e468f4c2123e63bbde7ac17ae1c279cdb627
Ilya Skumin <ilya_skumin@epam.com>
2023-01-20 15:28:10 +0300

    update configuration

--- post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/repository/PostRepository.java
+++ post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/repository/PostRepository.java
@@ -4,4 +4,5 @@ import com.ilyaskumin.k8scourse.postservice.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Long countAllByAuthorId(Long authorId);
 }
--- post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/service/PostService.java
+++ post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/service/PostService.java
@@ -1,4 +1,16 @@
 package com.ilyaskumin.k8scourse.postservice.service;
 
+import com.ilyaskumin.k8scourse.postservice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Long getPostCount(Long userId) {
+        return postRepository.countAllByAuthorId(userId);
+    }
 }
