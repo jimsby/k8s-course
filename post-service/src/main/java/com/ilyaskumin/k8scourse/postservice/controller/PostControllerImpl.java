1a3812b7cb8ee03e004089af46ec1de8bcc1ccdb
Ilya Skumin <ilya_skumin@epam.com>
2023-01-20 16:29:50 +0300

    add loging

--- post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/controller/PostControllerImpl.java
+++ post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/controller/PostControllerImpl.java
@@ -5,48 +5,55 @@ import com.ilyaskumin.k8scourse.postservice.model.dto.request.UpdatePostRequest;
 import com.ilyaskumin.k8scourse.postservice.model.dto.response.PostResponse;
 import com.ilyaskumin.k8scourse.postservice.repository.PostRepository;
 import com.ilyaskumin.k8scourse.postservice.service.PostMapper;
+import com.ilyaskumin.k8scourse.postservice.service.PostService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class PostControllerImpl implements PostController {
 
     private final PostMapper mapper;
     private final PostRepository repository;
+
+    private final PostService postService;
 
     @Override
     @Transactional
     public ResponseEntity<PostResponse> publishPost(CreatePostRequest createPostRequest) {
         var post = mapper.convert(createPostRequest);
         var savedPost = repository.save(post);
+        log.info("New post {} has created by user: {}", savedPost.getId(), savedPost.getAuthorId());
+        postService.updatePostCount(savedPost.getAuthorId());
         return ResponseEntity.ok(mapper.convert(savedPost));
     }
 
     @Override
     public ResponseEntity<PostResponse> getPost(Long id) {
-        var post = repository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post doesn’t exist with given id"));
+        var post = postService.findPost(id);
         return ResponseEntity.ok(mapper.convert(post));
     }
 
     @Override
     @Transactional
     public ResponseEntity<Void> deletePost(Long id) {
+        var authorId = postService.findPost(id).getAuthorId();
         repository.deleteById(id);
+        log.info("Post {} has deleted", id);
+        postService.updatePostCount(authorId);
         return ResponseEntity.ok().build();
     }
 
     @Override
     @Transactional
     public ResponseEntity<PostResponse> updatePost(Long id, UpdatePostRequest updatePostRequest) {
-        var post = repository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post doesn’t exist with given id"));
+        var post = postService.findPost(id);
         post.setText(updatePostRequest.text());
+        log.info("Text for post {} has updated", post.getId());
         return ResponseEntity.ok(mapper.convert(post));
     }
 }
--- post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/service/PostService.java
+++ post-service/src/main/java/com/ilyaskumin/k8scourse/postservice/service/PostService.java
@@ -1,16 +1,33 @@
 package com.ilyaskumin.k8scourse.postservice.service;
 
+import com.ilyaskumin.k8scourse.postservice.model.Post;
 import com.ilyaskumin.k8scourse.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
+
+    private static final String POST_NOT_EXIST_MESSAGE = "Post doesn’t exist with given id";
 
     private final PostRepository postRepository;
 
-    public Long getPostCount(Long userId) {
+    private Long getPostCount(Long userId) {
         return postRepository.countAllByAuthorId(userId);
+    }
+
+    public void updatePostCount(Long userId) {
+        var count = getPostCount(userId);
+        log.info("Count of posts for user {} changed to: {}", userId, count);
+    }
+
+    public Post findPost(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, POST_NOT_EXIST_MESSAGE));
     }
 }
